f6796c46c6319313a7c898b1ab3309da72c979f1
Paulina Szamraj <paulinaps.93@wp.pl>
2023-04-11 17:12:37 +0200

    added js formulas

--- js/script.js
+++ js/script.js
@@ -1,15 +1,26 @@
-console.log("Hej! Miło mi gościć Cię na mojej pierwszej stronie :)");
-
-let button = document.querySelector(".js-button");
-let background = document.querySelector(".background");
-let themeName = document.querySelector(".themeName");
-
-button.addEventListener("click", () => {
-    background.classList.toggle("darkBackground");
-
-    if(background.classList.contains("darkBackground")) {
-        themeName.innerText = "jasny";
-    } else {
-        themeName.innerText = "ciemny";
+{
+    const welcome = () => {
+        console.log("Hej! Miło mi gościć Cię na mojej pierwszej stronie :)");
     }
-});
+
+    const toggleBackground = () => {
+        const background = document.querySelector(".background");
+        const themeName = document.querySelector(".themeName");
+
+        background.classList.toggle("darkBackground");
+        if (background.classList.contains("darkBackground")) {
+            themeName.innerText = "jasny";
+        } else {
+            themeName.innerText = "ciemny";
+        }
+    };
+
+    const init = () => {
+        const button = document.querySelector(".js-button");
+        button.addEventListener("click", toggleBackground);
+
+        welcome();
+    };
+
+    init();
+}
